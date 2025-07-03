806fd3d9c866a1a5457eec52bc8befb1eed1b6e3
Henry Harborne <148478357+henryharborne@users.noreply.github.com>
2025-07-03 12:14:33 -0400

    login + dashboard boilerplate (#2)

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>This is the dashboard</h1>
+      <p>Congrats, you made it here!</p>
+    </div>
+  );
+}
